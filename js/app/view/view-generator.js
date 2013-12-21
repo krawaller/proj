fa0cfa247eb1249f62d179d0fb1671328b341cc0
ag222hd <ag222hd@student.lnu.se>
2013-12-21 11:47:00 +0100

    Update

--- js/app/view/view-generator.js
+++ js/app/view/view-generator.js
@@ -12,6 +12,7 @@ define([
     return Backbone.View.extend({
 
         initialize: function () {
+
             this.tg = new TemplateGenerator();
             var header = this.tg.render({el: this.tg.getHtmlId.HEADER, template: this.tg.getTemplate.HEADERTEMP}),
                 footer = this.tg.render({el: this.tg.getHtmlId.FOOTER, template: this.tg.getTemplate.FOOTERTEMP});
@@ -20,6 +21,7 @@ define([
             this.content = this.tg.render({el: this.tg.getHtmlId.MAINCONTENT, template: this.tg.getTemplate.MAINHOMETEMP});
         },
         renderApplicationPage: function () {
+            //this.content = this.tg.render({el: this.tg.getHtmlId.MAINCONTENT, template: this.tg.getTemplate.APPTEMP});
             this.content = new AppView({tg: this.tg});
         },
         renderAboutPage: function () {
