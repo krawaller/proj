8daf326e51d4631b65d05f83243f6b97e20686d8
ag222hd <ag222hd@student.lnu.se>
2013-12-21 11:02:01 +0100

    Fix

--- js/app/view/view-generator.js
+++ js/app/view/view-generator.js
@@ -12,7 +12,6 @@ define([
     return Backbone.View.extend({
 
         initialize: function () {
-
             this.tg = new TemplateGenerator();
             var header = this.tg.render({el: this.tg.getHtmlId.HEADER, template: this.tg.getTemplate.HEADERTEMP}),
                 footer = this.tg.render({el: this.tg.getHtmlId.FOOTER, template: this.tg.getTemplate.FOOTERTEMP});
@@ -21,7 +20,6 @@ define([
             this.content = this.tg.render({el: this.tg.getHtmlId.MAINCONTENT, template: this.tg.getTemplate.MAINHOMETEMP});
         },
         renderApplicationPage: function () {
-            //this.content = this.tg.render({el: this.tg.getHtmlId.MAINCONTENT, template: this.tg.getTemplate.APPTEMP});
             this.content = new AppView({tg: this.tg});
         },
         renderAboutPage: function () {
